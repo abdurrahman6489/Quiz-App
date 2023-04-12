afa1fb73058c0892b4cc489740e005013e8802cc
abdurrahman6489 <abdurrahman489@gmail.com>
2023-04-12 17:44:29 +0530

    pushing the new codes for Quiz App

--- app.js
+++ app.js
@@ -24,6 +24,22 @@ questionButton.forEach(btn=>{
         e.currentTarget.classList.add("active");
     })
 })
+function makeRandomNumber(lowest,highest){
+    return Math.floor(Math.random()*highest) + lowest;
+}
+// console.log(makeRandomNumber(1,100));
+function makeRandomIndexArray(array){
+    while(array.length<100){
+        let number = makeRandomNumber(1,100);
+        if(!array.includes(number)) array.push(number);
+    }
+    return array;
+}
+let indexArray = [];
+indexArray = makeRandomIndexArray(indexArray);
+console.log(indexArray);
+
+// indexArray.sort((a,b)=>(a-b));
 
 
 
